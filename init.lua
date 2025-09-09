f2ff9c2cf363b9ed630d4d6a7205ff6e169cdb20
Mike Wilkerson <wilkystyle@gmail.com>
2025-09-09 15:55:35 -0400

    Use <leader>f for true grepping

--- init.lua
+++ init.lua
@@ -268,7 +268,7 @@ require("lazy").setup({
     keys = {
       { "gd", function() require("fzf-lua").lsp_definitions() end, desc = "Goto LSP definitions using fzf" },
       { "grr", function() require("fzf-lua").lsp_references() end, desc = "Goto LSP references using fzf" },
-      { "<leader>f", function() require("fzf-lua").grep_project() end, desc = "grep/fuzzy-find in project with fzf" },
+      { "<leader>f", function() require("fzf-lua").live_grep() end, desc = "(rip)grep in project with fzf" },
       { "<leader>h", function() require("fzf-lua").helptags() end, desc = "Fuzzy search Neovim help topics with fzf" },
       { "<leader>l", function() require('fzf-lua').blines({previewer = false}) end, desc = "Fuzzy find lines in current buffer with fzf" },
       { "<leader>p", function() require("fzf-lua").files() end, desc = "Fuzzy-find files using fzf" },
