b4bf669368a3ee75d01dee5d1304fa19bf019f62
Mike Wilkerson <wilkystyle@gmail.com>
2025-09-09 10:44:08 -0400

    Add and configure vim-slime plugin

--- init.lua
+++ init.lua
@@ -273,6 +273,21 @@ require("lazy").setup({
   },
 
   {
+    'jpalardy/vim-slime',
+    keys = {
+      {"g<cr>", "<Plug>SlimeParagraphSend"},
+      {"g<cr>", "<Plug>SlimeRegionSend", mode="v" },
+    },
+    init = function()
+      vim.g.slime_no_mappings = 1
+      vim.g.slime_target = "tmux"
+      vim.g.slime_dont_ask_default = 1
+      vim.g.slime_bracketed_paste = 1
+      vim.g.slime_default_config = { socket_name = 'default', target_pane = '{last}' }
+    end,
+  },
+
+  {
     "mason-org/mason-lspconfig.nvim",
     opts = {},
     dependencies = {
