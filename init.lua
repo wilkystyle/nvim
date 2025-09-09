a76e1ec029713dda89b0f14ace25e34dec4d9890
Mike Wilkerson <wilkystyle@gmail.com>
2025-09-09 10:44:24 -0400

    Add and configure nvim-treesitter plugin

--- init.lua
+++ init.lua
@@ -296,6 +296,35 @@ require("lazy").setup({
     },
   },
 
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require 'nvim-treesitter.configs'.setup {
+        indent = { enable = true },
+        ensure_installed = {
+          "bash",
+          "c",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "query",
+          "rust",
+          "sql",
+          "vim",
+          "vimdoc",
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+                                              }
+    end,
+  },
+
 })
 
 
