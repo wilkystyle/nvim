1d9eb97be645a48f3da7b862b81bbca5972a204c
Mike Wilkerson <wilkystyle@gmail.com>
2025-09-09 10:40:15 -0400

    Remove commented lines in Oil plugin spec

--- init.lua
+++ init.lua
@@ -52,10 +52,7 @@ require("lazy").setup({
 
   {
     "stevearc/oil.nvim",
-    ---@module 'oil'
-    ---@type oil.SetupOpts
     opts = {},
-    -- Optional dependencies
     dependencies = { { "echasnovski/mini.icons", opts = {} } },
     lazy = false,
     keys = {
