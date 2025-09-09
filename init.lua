5a93c988491f3153031a46fbce654430ec6a1a49
Mike Wilkerson <wilkystyle@gmail.com>
2025-09-09 10:42:57 -0400

    Use mason-lspconfig for seamless LSP server enablement
    With this configuration, you no longer need to manually enable LSP
    servers in your Lua config, Mason will automatically enable any servers
    you have installed.

--- init.lua
+++ init.lua
@@ -272,7 +272,14 @@ require("lazy").setup({
     },
   },
 
-  { "mason-org/mason.nvim", opts = {} },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+  },
 
 })
 
