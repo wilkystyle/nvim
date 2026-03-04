c50ffc5d453fc7e4b9a6a9d9a0822a707bee66bb
Mike Wilkerson <wilkystyle@gmail.com>
2026-03-04 10:28:46 -0500

    Add markdown helper keybindings

---
+++ ftplugin/markdown.lua
@@ -0,0 +1,4 @@
+vim.keymap.set('n', '<leader>mk', [[ciW[<c-r>"]()<left>]])
+vim.keymap.set('n', '<leader>mb', [[ciW**<c-r>"**<esc>]])
+vim.keymap.set('n', '<leader>mi', [[ciW*<c-r>"*<esc>]])
+vim.keymap.set('n', '<leader>m`', [[ciW`<c-r>"`<esc>]])
