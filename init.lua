2d3d1db697b36acf9e8e15692b8f32e836b97dec
Mike Wilkerson <wilkystyle@gmail.com>
2026-08-27 20:27:59 -0400

    Scroll one line at a time with the mouse wheel

--- init.lua
+++ init.lua
@@ -42,6 +42,8 @@ end, { expr = true, noremap = true })
 ----------------------------------------------------------------------------------
 vim.opt.splitright = true                             -- New vertical splits should appear on the right, not the left!
 
+vim.opt.mousescroll = "ver:1,hor:1"                   -- Scroll one line at a time with the mouse wheel
+
 vim.opt.laststatus = 0                                -- Don't show the statusline
 vim.opt.showmode = false                              -- Don't show the mode in the message area
 vim.opt.ruler = false                                 -- Don't show line,col or position in file
